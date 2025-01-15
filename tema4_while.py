45f2009fff9954beb86b10564bc19ddeb8297cef
IDGS-805-22000980 <aguirrelesa@gmail.com>
2025-01-14 19:37:20 -0600

    Tablas de multiplicar

--- tema4_while.py
+++ tema4_while.py
@@ -1,9 +1,21 @@
-
+"""
 x=0
 
 while x<=10:
     print(x)
     x=x+1
+    
+"""
+
+
+
+cont=0
+print("dame un numero")
+x  = int(input("numero"))
+while cont<10:
+    cont=cont+1
+    result=x * cont
+    print(result)
     
 
 
